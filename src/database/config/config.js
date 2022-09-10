3657d0b8cd61586994155952306134bf4e0e44ff
Rei Greiq <greiqrei@gmail.com>
2022-09-10 23:57:40 +0700

    update config env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -36,7 +36,7 @@ const config = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,  
     dialect: "postgres",
     
   },
