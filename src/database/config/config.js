47cfee397c1a232cf7789e0cb7e95c9a99a857d0
greiq <jjanuar286@gmail.com>
2022-09-15 21:32:08 +0700

    env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,7 +7,8 @@ const config = {
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host : process.env.DB_HOST,
-    port : process.env.DB_PORT,        
+    port : process.env.DB_PORT,    
+    dialect: "postgres",
     url : process.env.DB_URL,
     dialectOptions: {
       ssl: {
@@ -15,7 +16,7 @@ const config = {
         rejectUnauthorized: false // This line will fix new error
       }
     },
-    // dialect: "postgres",
+    
   },
   test: {
     "username": "root",
@@ -25,39 +26,20 @@ const config = {
     "dialect": "mysql"
   },
   production: {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,    
+    dialect: "postgres",
+    url : process.env.DB_URL,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
+    },
   }
 }
 
 module.exports = config;
-
-// const bcrypt = require("bcrypt");
-
-// async function hashHost() {
-//  const hashHost = await bcrypt.hash(process.env.DB_HOST, 10);
-//  return hashHost
-// }
-
-// async function hashUser() {
-//   const hashUser = await bcrypt.hash(process.env.DB_USER, 10);
-//   return hashUser
-//  }
-
-//  async function hashDatabse() {
-//   const hashDatabse= await bcrypt.hash(process.env.DB_NAME, 10);
-//   return hashDatabse
-//  }
-
-//  const databaseencript = hashDatabse;
-
-// console.log(hashDatabse);
-
-  // DATABASE_URL : process.env.DATABASE_URL,
-
-      // port : process.env.DB_PORT,  
-      //   URI : process.env.DB_URI,
-    // HerokuCLI : process.env.DB_CLI,
